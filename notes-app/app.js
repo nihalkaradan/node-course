bd6e37b54325994c3bc040aff293247df93bed64
Nihal Karadan <nihalkaradan@gmail.com>
2021-06-24 07:58:40 +0530

    notes app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,56 @@
-const getNotes = require('./notes');
-console.log(getNotes());
-// const fs = require('fs');
-// fs.appendFileSync('notes.txt','hello world dbfudsjkfjdn \n')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const note = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        note.addNotes(argv.body,argv.title)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+
+yargs.parse()
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,29 @@
+const fs = require("fs");
 const getNotes = () => {
   return "your notes ...";
 };
-module.exports = getNotes;
+const addNotes = (body, title) => {
+  const notes = loadNote();
+  notes.push({
+    title: title,
+    body: body,
+  });
+  saveNote(notes);
+  // console.log(notes)
+};
+const saveNote = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+const loadNote = () => {
+  try {
+    const dataJSON = fs.readFileSync("notes.json").toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+module.exports = {
+  getNotes: getNotes,
+  addNotes: addNotes,
+};
